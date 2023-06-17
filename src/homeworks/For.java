1b094f592211608a8b31daafe584d92bd2426c16
Valerii1129 <yanovskiyvalera1@gmail.com>
2023-06-17 10:23:32 +0100

    lessons1

--- src/homeworks/For.java
+++ src/homeworks/For.java
@@ -1,2 +1,13 @@
-package homeworks;public class For {
+package homeworks;
+import java.util.Arrays;
+import java.util.Random;
+public class For {
+    public static void main(String[] args) {
+        int[] n = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++) {
+            n[i] = rnd.nextInt(10);
+        }
+        System.out.println(Arrays.toString(n));
+    }
 }
