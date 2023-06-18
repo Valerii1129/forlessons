9471c56c540a2b12bc007ab701103fdf38ecc307
Valerii1129 <yanovskiyvalera1@gmail.com>
2023-06-18 21:13:34 +0100

    commit for lesson3.5

--- src/lessons3/Cat.java
+++ src/lessons3/Cat.java
@@ -3,11 +3,11 @@ public class Cat extends Animal{
 private String name;
     @Override
     void eat() {
+        //void eat
         System.out.println("(Cat eat milk)");
         System.out.println("Cat say:Thanks my host");
     }
     public void setName(String name) {
         this.name = name;
     }
-
 }
--- src/lessons3/Dog.java
+++ src/lessons3/Dog.java
@@ -3,6 +3,7 @@ public class Dog extends Animal{
     private String name;
     @Override
     void eat() {
+        //void eat
         System.out.println("(Dog eat maet)");
         System.out.println("Dog say: Thanks my host!");
     }
