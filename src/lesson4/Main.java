1c3d4e95617f881620c1cd8e8bd8a9490892e5a0
Valerii1129 <yanovskiyvalera1@gmail.com>
2023-06-21 21:00:14 +0100

    Third commit

--- src/lesson4/Main.java
+++ src/lesson4/Main.java
@@ -8,7 +8,6 @@ public class Main {
         Privated mas2 = new Privated(new char[]{'a', 'b', 'c', 'd'});
         String[] names2 = new String[]{"Tolya", "Vladic", "Kolya"};
         char[] symvol2 = new char[]{'e', 'f', 'g', 'h'};
-
         System.out.println(mas);
         System.out.println(mas2);
         System.out.println(Arrays.toString(names2));
